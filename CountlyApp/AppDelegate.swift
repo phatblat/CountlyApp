e1568227c1c4fa0521b59b61496c98c761c07119
Ben Chatelain <ben@octop.ad>
2018-03-29 11:47:08 -0600

    📈 Confligure Countly immediately after launch

--- CountlyApp/AppDelegate.swift
+++ CountlyApp/AppDelegate.swift
@@ -6,41 +6,23 @@
 //  Copyright © 2018 Log Dash G, LLC. All rights reserved.
 //
 
+import Countly
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let config = CountlyConfig()
+        config.appKey = "220e1e4062c98195840da4754f84fa528f049429"
+        config.host = "https://kp.count.ly"
+        config.features = [CLYPushNotifications, CLYCrashReporting, CLYAutoViewTracking]
+
+        Countly.sharedInstance().start(with: config)
+        Countly.sharedInstance().askForNotificationPermission()
+
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
